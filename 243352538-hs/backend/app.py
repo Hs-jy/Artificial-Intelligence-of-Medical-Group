dbb6575d8dddcc9840b316a2f3e6a126bdf6dbea
Hs-jy <1084413803@qq.com>
2025-09-08 20:10:28 +0800

    Add files via upload

---
+++ 243352538-hs/backend/app.py
@@ -0,0 +1,177 @@
+import joblib
+import numpy as np
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import json
+import shap
+from matplotlib import pyplot as plt
+import os
+
+app = Flask(__name__,static_url_path='/static')
+CORS(app, resources={r"/api/*": {"origins": "http://47.101.189.188:8000"}})  # 允许跨域请求
+
+# 加载训练好的模型
+model = joblib.load(open("Machine_best_model_18.pkl", "rb"))
+
+# 假设类标签列表
+treatmusic = ["Gong", "Shang", "Jue", "Zhi", "Yu"]
+classes = ["WSS", "LFBU", "PFSI", "WFAI", "KED"]
+
+treatmusic_zh = ["宫乐", "商乐", "角乐", "徵乐", "羽乐"]
+classes_zh = ["脾胃虚弱", "肝火上扰", "痰火郁结", "风热侵袭", "肾精亏损"]
+
+@app.route("/")  # 根路由
+def home():
+    return "Flask server is running."
+
+# @app.route("/predict", methods=["POST"])  # 处理预测请求
+@app.route("/api/predict", methods=["GET"])  # 处理预测请求
+def predict():
+    try:
+        # 获取用户输入的特征，并将其转换为整数
+        # data = request.json #request.json是一个字典对象
+        # int_features = data['features']
+        # front_language = data['language']
+        # print(front_language)
+        # final_features = np.array(int_features).reshape(1, -1)  # 将特征数据转化为二维数组供模型输入
+
+        # 从查询参数中获取 features 和 language
+        features = request.args.get('features')
+        front_language = request.args.get('language')
+    
+        print(f'get{features}')
+
+        if features is None or front_language is None:
+            return jsonify({"error": "Missing 'features' or 'language' parameter"}), 400
+
+        # 尝试将 features 从字符串转换为 JSON 对象
+        int_features = json.loads(features)  # 确保 features 是一个有效的 JSON 字符串
+
+        final_features = np.array(int_features).reshape(1, -1)  # 将特征数据转化为二维数组供模型输入
+
+        # 如果模型期望有18个特征，且我们收到的输入特征少于18个，可以填充剩余的特征为0
+        if final_features.shape[1] < 18:
+            final_features = np.pad(final_features, ((0, 0), (0, 18 - final_features.shape[1])), 'constant')
+
+        print(final_features)
+        # 使用模型进行预测
+        prediction = model.predict(final_features)
+        prediction_proba = model.predict_proba(final_features)
+
+        # 获取预测结果的类别
+        output = prediction[0]
+        print(output)
+    
+        if front_language == 'English':
+            feature_names_en = [
+                # 'Tinnitus like cicada calls worsens at night',
+                'Low pitched tinnitus occurred within one month',
+                'History of cold or chronic rhinitis',
+                'Tinnitus sounds like roaring wind or tide',
+                'Restless insomnia with early morning awakening',
+                'Irritability insomnia and vivid dreams',
+                'Lower back pain and nocturnal emission',
+                'Impulsive and irritable personality',
+                'Heaviness in the head bitter or bland taste in the mouth',
+                'Sensation of emptiness in the ear',
+                'Ear fullness and blockage causing breathlessness',
+                'Worsens when standing up',
+                'Worsens after exertion',
+                # 'Thin tongue coating',
+                'Greasy tongue coating',
+                'Yellow tongue coating',
+                'Floating pulse',
+                'Wiry pulse',
+                'Slippery pulse',
+                'Thin pulse']
+
+            #shap预测
+            explainer = shap.TreeExplainer(model,feature_names=feature_names_en)
+            shap_values = explainer(final_features)
+
+            print(shap_values.shape)
+            print(shap_values)
+
+            # 生成SHAP Waterfall图
+            shap_fig = shap.plots.waterfall(shap_values[0,:,output],show = False)
+            fig = plt.gcf()
+            ax = plt.gca()
+
+            fig.set_size_inches(10,7)
+            plt.title(f'Waterfall plot for {classes[output]}',fontsize = 14)
+            plt.tight_layout()
+            plt.savefig(f'static/waterfall_plot_en.png',dpi = 300)
+            plt.close()
+        else:
+            # 保存当前的 rcParams 设置
+            original_rcParams = plt.rcParams.copy()
+            # 临时设置中文字体
+            plt.rcParams['font.sans-serif'] = ['SimHei']  # 用黑体显示中文
+            plt.rcParams['axes.unicode_minus'] = False  # 正确显示负号
+
+            feature_names_zh = [
+                # '耳鸣如蝉鸣，夜间加重',
+                '耳鸣声低沉，耳鸣发作一月内',
+                '有感冒史，或素有鼻炎',
+                '耳鸣轰轰如风声、潮声',
+                '虚烦失眠，晨起早醒',
+                '烦躁失眠，多梦',
+                '腰酸，遗精',
+                '性格急躁、易怒',
+                '头昏沉重，口苦或口淡',
+                '耳内空虚感',
+                '耳胀，耳闭塞憋气',
+                '站起时加重',
+                '劳作后加重',
+                # '薄（苔）',
+                '腻（苔）',
+                '黄（苔）',
+                '浮（脉）',
+                '弦（脉）',
+                '滑（脉）',
+                '细（脉）']
+            #shap预测
+            explainer = shap.TreeExplainer(model,feature_names=feature_names_zh)
+            shap_values = explainer(final_features)
+
+            print(shap_values.shape)
+            print(shap_values)
+
+            # 生成SHAP Waterfall图
+            shap_fig = shap.plots.waterfall(shap_values[0,:,output],show = False)
+            fig = plt.gcf()
+            ax = plt.gca()
+
+            fig.set_size_inches(10,7)
+            plt.title(f'{classes_zh[output]}的SHAP瀑布图',fontsize = 14)
+            plt.tight_layout()
+            plt.savefig(f'static/waterfall_plot_zh.png',dpi = 300)
+            plt.close()
+            # 恢复原来的 rcParams 设置
+            plt.rcParams.update(original_rcParams)
+
+        probabilities = [{"class": cls, "probability": prob} for cls, prob in zip(classes, prediction_proba[0])]
+        probabilities_zh = [{"class": cls, "probability": prob} for cls, prob in zip(classes_zh, prediction_proba[0])]
+
+        audio_url = f"http://47.101.189.188:5000/static/music/{output}.mp3"
+        img_url_en = f"http://47.101.189.188:5000/static/waterfall_plot_en.png"
+        img_url_zh = f"http://47.101.189.188:5000/static/waterfall_plot_zh.png"
+
+        print(classes_zh[output])
+        print(treatmusic_zh[output])
+        response = {
+            "prediction": classes[output] if front_language == 'English' else classes_zh[output],
+            "treatment":treatmusic[output] if front_language == 'English' else treatmusic_zh[output],
+            "probabilities":probabilities if front_language == 'English' else probabilities_zh,
+            "audio_url":audio_url,
+            "img_url":img_url_en if front_language == 'English' else img_url_zh,
+        }
+
+        return jsonify(response)
+
+    except Exception as e:
+        # 捕获异常并返回错误信息
+        return jsonify({"error": str(e)}), 400
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port=5000,debug=True)
---
+++ 243352538-hs/frontend/PredictionForm.vue
@@ -0,0 +1,496 @@
+<template>
+  <div id="vanta-background" class="vanta-container">
+  <div class="container">
+
+    <!-- 标题 -->
+     <div class="title-style">
+      <h1 v-if="language === 'English'" class="title">Subjective Tinnitus Online Diagnosis and Treatment System</h1>
+      <h1 v-if="language === '中文'" class="title">原发性耳鸣在线诊疗系统</h1>
+      <div class="config-box">
+        <button @click="changeLanguage" class="config-change-language">{{ oppositeLanguage }}</button>
+      </div>
+     </div>
+
+    <div class="content">
+      <!-- 左侧：症状选择框 -->
+      <div class="symptom-box">
+        <h2 v-if="language === 'English'" class="subtitle">Please Select Symptoms:</h2>
+        <h2 v-if="language === '中文'" class="subtitle">请选择您的症状:</h2>
+
+        <div class="symptoms">
+          <div
+            v-for="feature in currentFeatures"
+            :key="feature.id"
+            class="symptom-item"
+            :class="{ selected: feature.value === '1' }"
+            @click="toggleFeature(feature)"
+          >
+            {{ feature.label }}
+          </div>
+        </div>
+      </div>
+
+      <!-- 右侧：诊断结果与音乐播放器 -->
+      <div class="result-box">
+        
+        <p v-if="predictionText" class="prediction-text" v-html="predictionText"></p>
+
+        <!-- <img :src="imgUrl" alt="" v-if="imgUrl"> -->
+
+        <!-- <p v-if="audioUrl && language === 'English'" class="audio-label">Treatment Audio:</p>
+        <p v-if="audioUrl && language === '中文'" class="audio-label">治疗音乐:</p> -->
+
+        <!-- 音频播放器组件 -->
+        <AudioPlayer :audioURL="audioUrl" v-if="audioUrl" />
+
+        <button v-if="language === 'English'" @click="submitForm" class="submit-btn">Diagnose</button>
+        <button v-if="language === '中文'" @click="submitForm" class="submit-btn">诊断</button>
+
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import axios from "axios";
+import 'kursor/dist/kursor.css';
+import Kursor from 'kursor';
+import AudioPlayer from './AudioPlayer.vue'//导入AudioPlayer.vue组件
+// import * as THREE from "three";
+// import NET from "vanta/dist/vanta.net.min";
+import { defineComponent } from 'vue';
+
+
+export default defineComponent({
+  name: 'PredictionForm',
+  data() {
+    return {
+      language:'English',
+      features:this.getDaultFeatures(),
+      features_zh:this.getFeaturesZh(),
+      predictionText: "",
+      audioUrl:"", //存储音频url
+      vantaEffect:null, // 用于存储 Vanta.js 实例
+      imgUrl:"",
+    };
+  },
+  computed:{
+    currentFeatures() {
+      return this.language === 'English' ? this.features : this.features_zh;
+    },
+    oppositeLanguage(){
+      return this.language === 'English' ? '中文版' : 'English Version';
+    }
+  },
+  methods: {
+    toggleFeature(feature) {
+      feature.value = feature.value === "0" ? "1" : "0";
+    },
+    getDaultFeatures(){
+      return [
+        {'id': 'feature1', 'label': 'Low pitched tinnitus occurred within one month', 'value': '0'},
+        {'id': 'feature2', 'label': 'History of cold or chronic rhinitis', 'value': '0'},
+        {'id': 'feature3', 'label': 'Tinnitus sounds like roaring wind or tide', 'value': '0'},
+        {'id': 'feature4', 'label': 'Restless insomnia with early morning awakening', 'value': '0'},
+        {'id': 'feature5', 'label': 'Irritability insomnia and vivid dreams', 'value': '0'},
+        {'id': 'feature6', 'label': 'Lower back pain and nocturnal emission', 'value': '0'},
+        {'id': 'feature7', 'label': 'Impulsive and irritable personality', 'value': '0'},
+        {'id': 'feature8', 'label': 'Heaviness in the head bitter or bland taste in the mouth', 'value': '0'},
+        {'id': 'feature9', 'label': 'Sensation of emptiness in the ear', 'value': '0'},
+        {'id': 'feature10', 'label': 'Ear fullness and blockage causing breathlessness', 'value': '0'},
+        {'id': 'feature11', 'label': 'Worsens when standing up', 'value': '0'},
+        {'id': 'feature12', 'label': 'Worsens after exertion', 'value': '0'},
+        {'id': 'feature13', 'label': 'Greasy tongue coating', 'value': '0'},
+        {'id': 'feature14', 'label': 'Yellow tongue coating', 'value': '0'},
+        {'id': 'feature15', 'label': 'Floating pulse', 'value': '0'},
+        {'id': 'feature16', 'label': 'Wiry pulse', 'value': '0'},
+        {'id': 'feature17', 'label': 'Slippery pulse', 'value': '0'},
+        {'id': 'feature18', 'label': 'Thin pulse', 'value': '0'}
+        ]
+    },
+    getFeaturesZh(){
+      return [
+        {'id': 'feature1', 'label': '耳鸣声低沉，耳鸣发作一月内', 'value': '0'},
+        {'id': 'feature2', 'label': '有感冒史，或素有鼻炎', 'value': '0'},
+        {'id': 'feature3', 'label': '耳鸣轰轰如风声、潮声', 'value': '0'},
+        {'id': 'feature4', 'label': '虚烦失眠，晨起早醒', 'value': '0'},
+        {'id': 'feature5', 'label': '烦躁失眠，多梦', 'value': '0'},
+        {'id': 'feature6', 'label': '腰酸，遗精', 'value': '0'},
+        {'id': 'feature7', 'label': '性格急躁、易怒', 'value': '0'},
+        {'id': 'feature8', 'label': '头昏沉重，口苦或口淡', 'value': '0'},
+        {'id': 'feature9', 'label': '耳内空虚感', 'value': '0'},
+        {'id': 'feature10', 'label': '耳胀，耳闭塞憋气', 'value': '0'},
+        {'id': 'feature11', 'label': '站起时加重', 'value': '0'},
+        {'id': 'feature12', 'label': '劳作后加重', 'value': '0'},
+        {'id': 'feature13', 'label': '腻（苔）', 'value': '0'},
+        {'id': 'feature14', 'label': '黄（苔）', 'value': '0'},
+        {'id': 'feature15', 'label': '浮（脉）', 'value': '0'},
+        {'id': 'feature16', 'label': '弦（脉）', 'value': '0'},
+        {'id': 'feature17', 'label': '滑（脉）', 'value': '0'},
+        {'id': 'feature18', 'label': '细（脉）', 'value': '0'}
+        ]
+    },
+    changeLanguage(){
+      this.language = this.language == 'English' ? '中文' : 'English';
+      //将输出结果和音频还回默认
+      this.predictionText = '';
+      this.audioUrl = '';
+      this.imgUrl = '';
+      //每次点击换语言就将特征还原回默认
+      this.features = this.getDaultFeatures();
+      this.features_zh = this.getFeaturesZh();
+    },
+    submitForm() {
+      const features = this.language === 'English' ? this.features.map((f) => parseInt(f.value)) : this.features_zh.map((f) => parseInt(f.value));
+      const language = this.language;
+      this.predictionText = "Loading...";
+      this.audioUrl = "";
+      this.imgUrl = "";
+
+      axios
+        //.post("http://localhost:5000/predict", { features, language })
+        // .post("http://121.4.79.250/api/predict", { features, language })
+        // .get("http://121.4.79.250/api/predict", { 
+        .get("http://47.101.189.188:5000/api/predict", { 
+          params:{
+            features: JSON.stringify(features), 
+            language: language,
+            timestamp: Date.now() 
+          },
+          headers: {'Content-Type': 'application/json'}})
+        .then((response) => {
+          const data = response.data;
+          if(language == 'English'){
+            this.predictionText = `
+            <h2 class="subtitle">Diagnosis Result</h2>
+            <span class="large-text">Likely class: <span style="color: red;">${data.prediction}</span></span><br>
+            <span class="medium-text">Probability for each class:</span><br>
+            ${data.probabilities
+              .sort((a, b) => b.probability - a.probability) // 按概率从高到低排序
+              .map((p) => `<span class="medium-text">${p.class}: ${(p.probability*100).toFixed(0)}%</span>`)
+              .join("<br>")}
+            <br>
+            <a href="${data.img_url}?timestamp=${Date.now()}" target="_blank">
+            <img src="${data.img_url}?timestamp=${Date.now()}" alt="SHAP Plot" class="result-image" />
+            </a>
+            <p class="tips">click on the image to view details</p>
+            <span class="large-text">Treatment Music: <span style="color: red;font-size=40">${data.treatment}</span></span>`;
+          }
+          else{
+            this.predictionText = `
+            <h2 class="subtitle">诊断结果</h2>
+            <span class="large-text">症型判别: <span style="color: red;">${data.prediction}</span></span><br>
+            <span class="medium-text">每种症型的可能性:</span><br>
+            ${data.probabilities
+              .sort((a, b) => b.probability - a.probability) // 按概率从高到低排序
+              .map((p) => `<span class="medium-text">${p.class}: ${(p.probability*100).toFixed(0)}%</span>`)
+              .join("<br>")}
+            <br/>
+            <a href="${data.img_url}?timestamp=${Date.now()}" target="_blank">
+            <img src="${data.img_url}?timestamp=${Date.now()}" alt="SHAP Plot" class="result-image" />
+            </a>
+            <br/>
+            <p class="tips">点击图片查看详情</p>
+            <span class="large-text">治疗方案: <span style="color:red;">${data.treatment}</span></span>`;
+          }
+          this.audioUrl = data.audio_url; //设置从接口获取音频的URL
+          this.imgUrl =  `${data.img_url}?timestamp=${Date.now()}`; // 添加时间戳 ,设置shap_waterfall_plot图
+          // this.imgUrl = data.img_url; //设置从接口获取图片的URL,设置shap_waterfall_plot图
+        })
+        .catch((error) => {
+          this.predictionText = `<span class="medium-text" style="color: red;">Error: ${error.response.data.error}</span>`;
+        });
+        // console.log(features);
+        // console.log(language);
+    },
+  },
+  async mounted(){
+      document.title = 'Tinnitus Online Diagnosis';
+      //初始化kursor
+      new Kursor({ 
+        type: 2,
+        color:'#F59F9F'
+      });
+      // 初始化 Vanta.js
+      // if(window.innerWidth > 768){
+      //   this.vantaEffect = NET({
+      //     el: "#vanta-background",
+      //     mouseControls: true,
+      //     touchControls: true,
+      //     gyroControls: false,
+      //     minHeight: 200.0,
+      //     minWidth: 200.0,
+      //     scale: 1.0,
+      //     scaleMobile: 1.0,
+      //     backgroundColor: 0xf3f3f4,
+      //     maxDistance: 13.0,
+      //     spacing: 13.0,
+      //     THREE, // 必须传递 Three.js 依赖
+      //     vertexColors: false, // 添加 vertexColors 参数
+      //   });
+      // }
+    },
+  beforeDestroy() {
+    if (this.vantaEffect && typeof this.vantaEffect.destroy === "function") {
+      this.vantaEffect.destroy();
+      this.vantaEffect = null;
+    }
+  },
+    // 将组件在 components 配置项中进行注册
+  components: {
+    AudioPlayer,
+  },
+});
+</script>
+
+<style>
+.vanta-container {
+  margin: 0;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+/* 全局样式 */
+.container {
+  font-family: Arial, sans-serif;
+  padding: 20px;
+  width: 80%;
+  max-width: 1300px;
+  height: 90%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* background-color:beige; */
+  /* box-sizing: border-box; */
+}
+.title {
+  text-align: center;
+  font-size: 35px;
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 20px;
+}
+.title-style{
+  position: relative;
+  width: 100%;
+  text-align: center;
+  flex-direction: row;
+  justify-content: center;
+  margin-bottom: 5px;
+}
+.subtitle{
+  font-size: 30px;
+  margin-bottom: 20px;
+  margin-top: auto;
+}
+/* 内容区域 */
+.content {
+  display: flex;
+  justify-content: space-between;
+  gap: 20px;
+  align-items: flex-start;
+  height: 100%;
+}
+
+/* 左侧症状框 */
+.symptom-box {
+  flex: 3;
+  background-color: #f9f9f9;
+  padding: 20px;
+  border-radius: 8px;
+  height: 100%;
+  width: 90%;
+}
+.symptoms {
+  display: grid;
+  grid-template-columns: repeat(3,1fr);
+  grid-gap:15px;
+  grid-auto-rows: 100px;
+}
+.symptom-item {
+  display: flex;
+  background: #e0e0e0;
+  padding: 20px;
+  border-radius: 10px;
+  align-items: center;
+  text-align: center;
+  justify-content: center;
+  font-size: 20px;
+  cursor: pointer;
+  transition: box-shadow 0.3s ease;
+  min-height: 60px;
+}
+.symptom-item.selected {
+  /* box-shadow: 0 0 8px #4caf50; */
+  background-color: rgb(100, 167, 169);
+}
+
+/* 右侧结果框 */
+.result-box {
+  flex: 1;
+  background-color: #f9f9f9;
+  padding: 20px;
+  border-radius: 8px;
+  text-align: center;
+  height: 100%;
+  min-height: 500px;
+  min-width: 380px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+}
+.prediction-text {
+  margin-bottom: 15px;
+  /* padding: 10px; */
+  /* text-align: left; */
+}
+.submit-btn {
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  padding: 20px 30px;
+  border-radius: 8px;
+  cursor: pointer;
+  font-size: 18px;
+  margin-top: auto;
+}
+.submit-btn:hover {
+  background-color: #45a049;
+}
+.large-text{
+  font-size: 30px;
+  margin-top: auto;
+  margin-bottom: 8px;
+}
+.medium-text{
+  font-size: 25px;
+}
+.small-text{
+  font-size: 20px;
+}
+.tips{
+  font-size: 15px;
+  color: #45a049;
+}
+/* 音乐播放器 */
+.music-player {
+  margin-top: 20px;
+}
+.controls {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+.progress-container,
+.volume-container {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  margin-top: 10px;
+}
+
+.progress-bar,
+.volume-bar {
+  flex: 1;
+  cursor: pointer;
+  height: 5px;
+}
+
+/* 音频标签样式 */
+.audio-label {
+  font-size: 25px;
+  color: #333;
+  margin-top: 15px;
+}
+
+/* 音频播放器样式 */
+.audio-player {
+  width: 100%;
+  margin-top: 5px;
+}
+.result-image {
+  width: 100%;
+
+}
+/* 配置面板 */
+.config-box {
+  position: absolute;
+  right: 0;
+  bottom: 0;
+  /* right: 25.2vw;
+  top:110px; */
+}
+.config-change-language {
+  background-color: #f9f9f9;
+  color: black;
+  padding: 10px 20px;
+  border-radius: 8px;
+  font-size: 16px;
+  margin-top: auto;
+  border-style: none;
+}
+@media (max-width: 768px){
+  .content {
+    flex-direction: column;
+    align-items: stretch;
+  }
+  .symptoms {
+    grid-template-columns: repeat(1,1fr);
+    grid-auto-rows: auto;
+  }
+  .symptom-item {
+    font-size: 20px;
+    min-height: auto;
+  }
+  .symptom-box,
+  .result-box {
+    height: auto;
+    width: 100%;
+    flex: none;
+    min-width: 350px;
+  }
+  .result-box{
+    min-width: auto;
+    min-height: auto;
+  }
+  .config-change-language{
+    font-size: 12px;
+    padding: 8px 15px;
+    margin: auto;
+  }
+  .title{
+    font-size: 30px;
+  }
+  .subtitle {
+    font-size: 25px;
+  }
+  .title-style{
+    min-width: 350px;
+  }
+}
+/* @media (min-width: 769px) and (max-width: 1025px) {
+  .symptom-item{
+    font-size: 15px;
+  }
+  .symptom-box{
+    min-width: 600px;
+  }
+  .subtitle {
+    font-size: 20px;
+  }
+  .large-text,.audio-label{
+    font-size: 20px;
+  }
+  .medium-text{
+    font-size: 15px;
+  }
+  .result-box{
+    min-width: 250px;
+  }
+} */
+</style>
